83ada3ef985afaa14a90cc25f3dc1abb6949bdb8
Elba <elba.duishekeeva16@gmail.com>
2023-12-05 22:41:48 +0600

    todo-redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux'
+import {addTodo, deleteTodo, editTodo, getTodos} from "./redux/action/todoAction";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const dispatch = useDispatch()
+    const todosArray = useSelector(state => state.todos)
+    const [todo, setTodo] = useState({})
+    const [edit, setEdit] = useState(false)
+
+    useEffect(() => {
+        dispatch(getTodos(todos))
+    }, []);
+
+    const handeAddTodo = () => {
+        const data = {id: todosArray.length + 1, title: todo.title, completed: false}
+        dispatch(addTodo(data))
+        setTodo({})
+    }
+
+    const handleDelete=(id)=> {
+        dispatch(deleteTodo(id))
+    }
+
+    const handleEdit=(todo)=> {
+        dispatch(editTodo(todo))
+    }
+
+    const handleCompleted=()=> {
+    }
+    return (
+        <div>
+            <input type="text" value={todo.title || ''} onChange={(e) => setTodo({...todo, title: e.target.value})}/>
+            <button onClick={handeAddTodo}>Add</button>
+
+            {
+                todosArray.map(todo =>
+                    <div key={todo.id} style={{display: 'flex', gap: '10px', alignItems: 'center'}}>
+                        <h3>{todo.title}</h3>
+                        <input onChange={handleCompleted} type="checkbox" checked={todo.completed}/>
+                        <button onClick={() => handleEdit(todo)}>{edit?'Save':'Edit'}</button>
+                        <button onClick={()=>handleDelete(todo.id)}><i className='bx bxs-trash'></i></button>
+                    </div>
+                )
+            }
+        </div>
+    );
 }
 
 export default App;
+
+const todos = [
+    {
+        id: 1,
+        title: 'Buy milk',
+        completed: false
+    },
+    {
+        id: 2,
+        title: 'Buy eggs',
+        completed: false
+    },
+    {
+        id: 3,
+        title: 'Buy bread',
+        completed: false
+    },
+    {
+        id: 4,
+        title: 'Buy butter',
+        completed: false
+    }
+]
